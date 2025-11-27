ef4fff4c1341cd99abfbc3eb31d050d22c1848d5
Mark Gernitis <github@readestreet.email>
2025-11-27 01:59:02 -0800

    - add chunking, fix some bugs

--- src/model.py
+++ src/model.py
@@ -23,30 +23,34 @@ class DeepDietModel(nn.Module):
 
     """
 
-    def __init__(self, use_side_frames=True, use_overhead=True, use_depth=True):
+    def __init__(self, use_side_frames=True, use_overhead=True, use_depth=True, chunk_size=4, lstm_hidden=640):
         super().__init__()
+        self.use_side_frames = use_side_frames
+        self.use_overhead = use_overhead
+        self.use_depth = use_depth and use_overhead
+        self.chunk_size = chunk_size
+        self.lstm_hidden = lstm_hidden
 
         total_features = 0
 
-        if use_side_frames:
+        if self.use_side_frames:
             self.side_encoder = models.efficientnet_b0(weights=models.EfficientNet_B0_Weights.IMAGENET1K_V1)
             self.side_encoder.classifier = nn.Identity()
-            lstm_hidden = 640
-            self.side_aggregator = nn.LSTM(1280, lstm_hidden, batch_first=True, bidirectional=True)
-            total_features += lstm_hidden * 2
+            self.side_aggregator = nn.LSTM(1280, self.lstm_hidden, batch_first=True, bidirectional=True)
+            total_features += self.lstm_hidden * 2
 
-        if use_overhead:
+        if self.use_overhead:
             self.overhead_encoder = models.efficientnet_b0(weights=models.EfficientNet_B0_Weights.IMAGENET1K_V1)
             self.overhead_encoder.classifier = nn.Identity()
-            total_features += self.overhead_encoder.classifier[1].in_features
+            total_features += 1280
 
-        if use_depth:
+        if self.use_depth:
             self.depth_encoder = models.efficientnet_b0(weights=models.EfficientNet_B0_Weights.IMAGENET1K_V1)
             # Since pretrained weights are based on 3 channels, convert this to 1 channel by averaging the RGB channels.
             original_conv = self.depth_encoder.features[0][0]
             self.depth_encoder.features[0][0] = nn.Conv2d(1, original_conv.out_channels, kernel_size=original_conv.kernel_size, stride=original_conv.stride, padding=original_conv.padding, bias=False)
             self.depth_encoder.classifier = nn.Identity()
-            total_features += self.depth_encoder.classifier[1].in_features
+            total_features += 1280
 
         self.fusion_layers = nn.Sequential(
             nn.Linear(total_features, 1024),
@@ -78,17 +82,34 @@ class DeepDietModel(nn.Module):
             nn.Linear(128, 3)
         )
 
-    def forward(self, side_frames=None, overhead_rgb=None, overhead_depth=None):
+    def forward(self, side_frames, overhead_rgb=None, overhead_depth=None):
         features = []
 
-        if side_frames is not None:
-            side_feat, _ = self.side_aggregator(self.side_encoder(side_frames))
+        if self.use_side_frames and side_frames is not None:
+            batch_size = side_frames.size(0)
+            num_frames = side_frames.size(1)
+
+            # Flatten: [B, T, C, H, W] to [B * T, C, H, W]
+            side_frames_flat = side_frames.view(batch_size * num_frames, *side_frames.shape[2:])
+            side_features = []
+
+            for i in range(0, side_frames_flat.size(0), self.chunk_size):
+                chunk = side_frames_flat[i:i + self.chunk_size]
+                chunk_feat = self.side_encoder(chunk)
+                side_features.append(chunk_feat)
+
+            # Reshape for LSTM: [B * T, 1280] -> [B, T, 1280]
+            side_features = torch.cat(side_features, dim=0).view(batch_size, num_frames, -1)
+
+
+            side_feat, _ = self.side_aggregator(side_features)
+            side_feat = side_feat.mean(dim=1)
             features.append(side_feat)
 
-        if overhead_rgb is not None:
+        if self.use_overhead and overhead_rgb is not None:
             features.append(self.overhead_encoder(overhead_rgb))
 
-        if overhead_depth is not None:
+        if self.use_depth and overhead_depth is not None:
             features.append(self.depth_encoder(overhead_depth))
 
         concat_features = torch.cat(features, dim=1)
@@ -97,7 +118,4 @@ class DeepDietModel(nn.Module):
         calories = self.calorie_head(fused_features)
         macros = self.macro_head(fused_features)
 
-        return torch.cat([mass, calories, macros], dim=1)
-
-    def get_feature_dim(self):
-        return self.fusion_layers[-1].in_features
+        return torch.cat([calories, mass, macros], dim=1)
